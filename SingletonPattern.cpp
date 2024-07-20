fb56995fa350d163259f188ff2f38a32b461e6fe
lokesh.pancharia <lokesh.panchariya@gmail.com>
2024-07-20 15:13:31 +0530

    Reducing Run time overhead

--- SingletonPattern.cpp
+++ SingletonPattern.cpp
@@ -1,26 +1,22 @@
+#include <memory>
+#include <mutex>
+
 class Singleton {
 private:
-    static Singleton* instance;
-    
     // Private constructor to prevent instantiation
     Singleton() {}
-    
-    // Declare copy constructor and assignment operator as private
-    Singleton(const Singleton&) {}
-    Singleton& operator=(const Singleton&) {}
-    
+
+    // Delete copy constructor and assignment operator
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
 public:
     // Static method to get the instance of the Singleton class
-    static Singleton* getInstance() {
-        if (instance == nullptr) {
-            instance = new Singleton();
-        }
+    static Singleton& getInstance() {
+        static Singleton instance;
         return instance;
     }
-    
+
     // Other member functions of the Singleton class
     // ...
 };
-
-// Initializing the static instance variable
-Singleton* Singleton::instance = nullptr;
